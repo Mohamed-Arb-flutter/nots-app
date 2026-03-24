5729a7d1dee2252d2ce8965236d8afb7e90f8560
Mohamed <mohmedabrhem981@gmail.com>
2026-03-25 01:58:32 +0200

    craet type adaptor for note

--- lib/models/note-model.dart
+++ lib/models/note-model.dart
@@ -1,8 +1,21 @@
-class Notemodel {
+import 'package:hive/hive.dart';
+
+part 'note-model.g.dart';
+
+@HiveType(typeId: 0)
+class Notemodel extends HiveObject {
+  @HiveField(0)
   final String title;
+
+  @HiveField(1)
   final String subtitle;
+
+  @HiveField(2)
   final String date;
+
+  @HiveField(3)
   final int color;
+
   Notemodel({
     required this.title,
     required this.subtitle,
