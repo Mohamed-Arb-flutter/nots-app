7ac1a20ef727be4eefc5cb8bc7962c5d094282ca
Mohamed <mohmedabrhem981@gmail.com>
2026-04-01 18:56:43 +0200

    change Add loding

--- lib/widght/Add_Node_Form.dart
+++ lib/widght/Add_Node_Form.dart
@@ -38,22 +38,27 @@ class _AddNoteFormState extends State<AddNoteForm> {
             },
           ),
           const SizedBox(height: 100),
-          custombottom(
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-                var noteitem = Notemodel(
-                  title: title!,
-                  subtitle: subtitle!,
-                  date: DateTime.now().toString(),
-                  color: Colors.blue.value,
-                );
-                BlocProvider.of<AddNoteCubit>(context).AddNote(noteitem);
-              } else {
-                setState(() {
-                  autovalidateMode = AutovalidateMode.always;
-                });
-              }
+          BlocBuilder<AddNoteCubit, AddNoteState>(
+            builder: (context, state) {
+              return custombottom(
+                islooding: state is AddNoteLoding ? true : false,
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    var noteitem = Notemodel(
+                      title: title!,
+                      subtitle: subtitle!,
+                      date: DateTime.now().toString(),
+                      color: Colors.blue.value,
+                    );
+                    BlocProvider.of<AddNoteCubit>(context).AddNote(noteitem);
+                  } else {
+                    setState(() {
+                      autovalidateMode = AutovalidateMode.always;
+                    });
+                  }
+                },
+              );
             },
           ),
         ],
--- lib/widght/custom-bottom.dart
+++ lib/widght/custom-bottom.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:note_app/widght/constans.dart';
 
 class custombottom extends StatelessWidget {
-  const custombottom({super.key, this.onTap});
+  const custombottom({super.key, this.onTap, this.islooding = false});
   final void Function()? onTap;
+  final bool islooding;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -16,14 +17,20 @@ class custombottom extends StatelessWidget {
           color: kprimrycolor,
         ),
         child: Center(
-          child: Text(
-            'Add',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+          child: islooding
+              ? const SizedBox(
+                  height: 24,
+                  width: 24,
+                  child: CircularProgressIndicator(color: Colors.black),
+                )
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
         ),
       ),
     );
